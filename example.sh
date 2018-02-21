5bb16279dbd2d28a29bdd0d396ebc8ac1f198978
dgrisham <dgrisham@mines.edu>
2018-02-21 16:42:36 -0700

    Example test formats output as CSV.
    Note: Assumes user has PCRE `grep` support.
    
    License: MIT
    Signed-off-by: David Grisham <dgrisham@mines.edu>

--- example.sh
+++ example.sh
@@ -19,6 +19,18 @@ for i in {1..2}; do
     iptb run $i ipfs cat "$cid"
 done
 
-iptb for-each sh -c "ipfs id --format='id: <id>\n' && ipfs bitswap stat" > results
+# get header for bitswap stats
+echo -n "id," > results
+iptb run 0 sh -c "ipfs bitswap stat" | grep -oP '(?<=\t).*(?=:)' | tr ' ' '_' | paste -sd ',' >> results
+
+# gather stats for each node
+for i in {0..2}; do
+    iptb run $i sh -c "ipfs id --format='<id>,'" >> results
+    iptb run $i sh -c "ipfs bitswap stat" | grep -oP '(?<=: |\[)[0-9A-Za-z /]+(?=]|)' | paste -sd ',' >> results
+done
+
+# the following commented command should be able to grab bitswap stats and do
+# the relevant string manipulation, all on the docker image. but it's gross
+#iptb for-each sh -c "ipfs id --format='<id>,' && ipfs bitswap stat | grep ':\|\[' | sed 's/.*\?: //' | sed 's/.*\?\[\(.*\?\)\]/\1/' | paste -sd ',' -" > results
 
 iptb kill
