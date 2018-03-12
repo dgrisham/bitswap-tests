cafc01ac5a9850ecbd288bde920e7688aeda04b8
David Grisham <dgrisham@mines.edu>
2018-03-12 22:54:34 +0000

    Wrote template for full-connected test; added example result.
    1.  `example.sh` has been replaced by `template.sh`, which allows the user to
        specify the number of nodes to test, the command to use when creating the
        file that the users share, and the name of the output file. The template is
        for the category of tests that involve fully connected network of nodes
        where one node adds a file and the rest `cat` that file.
    2.  An example test result was added so that updates to the template can be
        checked by rerunning the test with the same input and checking that the
        output does not change. This test was generated with:
        `./template.sh -n 3 -c 'echo "hola, mundo"' -o results/holamundo` (this
        needs to be documented elsewhere).
    
    License: MIT
    Signed-off-by: David Grisham <dgrisham@mines.edu>

---
+++ template.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+set -ex
+
+# find better way to handle this arg count check
+[ ! $# -eq 6 ] && echo 'missing args' && exit 1
+while getopts ":n:c:o:" opt; do
+    case $opt in
+        n)
+            [[ -z "$OPTARG" ]] && exit 1
+            num_nodes="$OPTARG"
+            ;;
+        c)
+            [[ -z "$OPTARG" ]] && exit 1
+            creation_cmd="$OPTARG"
+            ;;
+        o)
+            [[ -z "$OPTARG" ]] && exit 1
+            results="$OPTARG"
+            ;;
+        /?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+        --)
+            break
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+yes | iptb init -n $num_nodes --type docker >/dev/null
+iptb start
+
+for ((i=0; i < num_nodes; i++)); do
+    for ((j=i+1; j < num_nodes; j++)); do
+        iptb connect $i $j
+    done
+done
+
+iptb run 0 sh -c "$creation_cmd >file"
+# add file to ipfs, save cid
+cid=$(iptb run 0 ipfs add -q ./file | tr -d '\r')
+
+# have each of the other nodes request the file
+for ((i=1; i < num_nodes; i++)); do
+    iptb run $i ipfs cat "$cid"
+done
+
+# get header for bitswap stats
+echo -n "id," > $results
+iptb run 0 sh -c "ipfs bitswap stat" | grep -oP '(?<=\t).*(?=:)' | tr ' ' '_' | paste -sd ',' >> $results
+
+# gather stats for each node
+for ((i=0; i < num_nodes; i++)); do
+    iptb run $i sh -c "ipfs id --format='<id>,'" >> $results
+    iptb run $i sh -c "ipfs bitswap stat" | grep -oP '(?<=: |\[)[0-9A-Za-z /]+(?=]|)' | paste -sd ',' >> $results
+done
+
+# the following commented command should be able to grab bitswap stats and do
+# the relevant string manipulation, all on the docker image. but it's gross
+#iptb for-each sh -c "ipfs id --format='<id>,' && ipfs bitswap stat | grep ':\|\[' | sed 's/.*\?: //' | sed 's/.*\?\[\(.*\?\)\]/\1/' | paste -sd ',' -" > $results
+
+iptb kill
