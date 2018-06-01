ef0714cb7ae3c9e28e457661f76a6bba5c491e98
David Grisham <dgrisham@mines.edu>
2018-06-01 17:43:42 +0000

    Added all test generation cases. Reran tests.

--- generate_test_results.sh
+++ generate_test_results.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 ./template.sh -n 3 -c 'echo "hola, mundo"' -o results/holamundo
-./template.sh -n 3 -c 'head -c 1000000 /dev/urandom' -o results/medium_random
+./template.sh -n 3 -c 'head -c 1000000000 /dev/urandom' -o results/random-medium
+./template.sh -n 3 -c 'head -c 10000000000 /dev/urandom' -o results/random-large
+
+./template.sh -n 3 -c 'echo "hola, mundo"' -o results/rrq/holamundo
+./template.sh -n 3 -c 'head -c 1000000000 /dev/urandom' -o results/rrq/random-medium
+./template.sh -n 3 -c 'head -c 10000000000 /dev/urandom' -o results/rrq/random-large
