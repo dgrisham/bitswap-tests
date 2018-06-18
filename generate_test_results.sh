453d94f5a218e15f2d1d1cfff554805212c1dff6
David Grisham <dgrisham@mines.edu>
2018-06-18 14:53:58 +0000

    Moving the generated rules for old PRQ.

--- generate_test_results.sh
+++ generate_test_results.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-./template.sh -n 3 -c 'echo "hola, mundo"' -d results/holamundo
-./template.sh -n 3 -c 'head -c 100000000 /dev/urandom' -d results/random-small
-./template.sh -n 3 -c 'head -c 1000000000 /dev/urandom' -d results/random-medium
-./template.sh -n 3 -c 'head -c 10000000000 /dev/urandom' -d results/random-large
+./template.sh -n 3 -c 'echo "hola, mundo"' -d results/prq/holamundo
+./template.sh -n 3 -c 'head -c 100000000 /dev/urandom' -d results/prq/random-small
+./template.sh -n 3 -c 'head -c 1000000000 /dev/urandom' -d results/prq/random-medium
+./template.sh -n 3 -c 'head -c 10000000000 /dev/urandom' -d results/prq/random-large
 
 ./template.sh -n 3 -s -c 'echo "hola, mundo"' -d results/rrq/holamundo
 ./template.sh -n 3 -s -c 'head -c 100000000 /dev/urandom' -d results/rrq/random-small
