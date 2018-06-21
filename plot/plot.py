07556f695dd128645132d526222fba3fb52ba7d7
dgrisham <dgrisham@mines.edu>
2018-06-20 21:22:20 -0600

    Still working on plotting.

--- plot/plot.py
+++ plot/plot.py
@@ -14,15 +14,15 @@ def main(argv):
     infile = argv[0]
     fname = os.path.basename(infile)
     freg = r'(?:(?P<dist>\d+(?:_\d+)+)|(?:(?P<hom_bw>\d+)x(?P<hom_num>\d+)))-aggregate'
-    parsed = re.match(freg, fname)
+    fmatch = re.match(freg, fname)
 
-    if parsed is None:
+    if fmatch is None:
         print("Bad filename format.")
         sys.exit(1)
-    elif parsed.group('dist') is not None:
-        dist = [int(b) for b in parsed.group('dist').split('_')]
-    elif parsed.group('hom_bw') is not None:
-        dist = [int(parsed.group('hom_bw'))] * int(parsed.group('hom_num'))
+    elif fmatch.group('dist') is not None:
+        resources = [int(b) for b in fmatch.group('dist').split('_')]
+    elif fmatch.group('hom_bw') is not None:
+        resources = [int(fmatch.group('hom_bw'))] * int(fmatch.group('hom_num'))
 
     data = pd.read_csv(infile, index_col=0)
     dl_times = {}
@@ -30,14 +30,18 @@ def main(argv):
     for peer, row in data.iterrows():
         ptime = re.match(treg, row['dl_time'])
         if ptime is not None:
-            seconds = int(ptime.group('m')) * 60 + int(ptime.group('s'))
-            dl_times[peer] = seconds
+            minutes = int(ptime.group('m')) if ptime.group('m') is not None else 0
+            dl_times[peer] = minutes * 60 + int(ptime.group('s'))
 
-    return dl_times, data
+    ratios = {}
+    for peer, time in dl_times.items():
+        ratios[peer] = (resources[peer] / sum(resources)) / (time / sum(dl_times.values()))
+
+    return ratios, data
     #results = pd.read_csv(fname, index_col=False)
     #results = results.set_index('peer')
     #return results
 
 if __name__ == '__main__':
     #main(sys.argv[1:])
-    dl_times, data = main(sys.argv[1:])
+    ratios, data = main(sys.argv[1:])
