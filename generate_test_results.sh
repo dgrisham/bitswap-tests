4e4e646daae93c56eab3a4e88caaf3151f53c70d
David Grisham <dgrisham@mines.edu>
2018-06-21 03:21:08 +0000

    More results.

--- generate_test_results.sh
+++ generate_test_results.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#./template.sh -n 3 -c 'echo "hola, mundo"' -d results/prq/holamundo
-#./template.sh -n 3 -c 'head -c 100000000 /dev/urandom' -d results/prq/random-small
-#./template.sh -n 3 -c 'head -c 1000000000 /dev/urandom' -d results/prq/random-medium
+./template.sh -n 3 -c 'echo "hola, mundo"' -d results/prq/holamundo
+./template.sh -n 3 -c 'head -c 100000000 /dev/urandom' -d results/prq/random-small
+./template.sh -n 3 -c 'head -c 1000000000 /dev/urandom' -d results/prq/random-medium
 #./template.sh -n 3 -c 'head -c 10000000000 /dev/urandom' -d results/prq/random-large
 
 ./template.sh -n 3 -s -c 'echo "hola, mundo"' -d results/rrq/holamundo
@@ -19,6 +19,11 @@
 ./template.sh -n 3 -s -b "10 20 10" -c 'head -c 100000000 /dev/urandom' -d results/rrq/random-small
 #./template.sh -n 3 -s -b "10 20 10" -c 'head -c 1000000000 /dev/urandom' -d results/rrq/random-medium
 #./template.sh -n 3 -s -b "10 20 10" -c 'head -c 10000000000 /dev/urandom' -d results/rrq/random-large
+
+./template.sh -n 3 -s -b "10 10 20" -c 'echo "hola, mundo"' -d results/rrq/holamundo
+./template.sh -n 3 -s -b "10 10 20" -c 'head -c 100000000 /dev/urandom' -d results/rrq/random-small
+#./template.sh -n 3 -s -b "10 10 20" -c 'head -c 1000000000 /dev/urandom' -d results/rrq/random-medium
+#./template.sh -n 3 -s -b "10 10 20" -c 'head -c 10000000000 /dev/urandom' -d results/rrq/random-large
 
 ./template.sh -n 3 -s -b "10 20 20" -c 'echo "hola, mundo"' -d results/rrq/holamundo
 ./template.sh -n 3 -s -b "10 20 20" -c 'head -c 100000000 /dev/urandom' -d results/rrq/random-small
