6bc73da4ec58a7870b30f9c063272e0f197c754a
David Grisham <dgrisham@mines.edu>
2018-10-04 04:05:53 +0000

    using run to test upload rate limits

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 set -ex
 
 # ./test.sh -t 2 -n 3 -s "identity" -r 10000 -f 'head -c 10000000 /dev/urandom' -d 'test-run'
-./test.sh -t 2 -n 3 -s "identity" -b 5000 -r 100000 -f 'head -c 10000000 /dev/urandom' -d 'test-run'
+# ./test.sh -t 2 -n 3 -s "identity" -b 5000 -r 100000 -f 'head -c 10000000 /dev/urandom' -d 'test-run'
+./test.sh -t 2 -n 3 -b '-1 -1 10000' -f 'head -c 10000000 /dev/urandom' -d 'bw-test-2'
--- tests/test-1.sh
+++ tests/test-1.sh
@@ -7,13 +7,19 @@
 
 body() {
     # connect nodes
-    iptb connect 0 "[1,$((num_nodes-1))]"
+    until iptb connect 0 "[1,$((num_nodes-1))]"; do :; done
 
     declare -a cids
+    declare -ag uploads
     for ((i=0; i < num_nodes; i++)); do
         iptb run $i -- sh -c "$file_cmd >file"
         # store cids of uploaded files
         cids[$i]=$(iptb run $i -- sh -c "ipfs add -q file && rm file" | tail -n2)
+        if [[ -z "${uploads[i]}" ]]; then
+            uploads[$i]='{"cid":"'"${cids[i]}"'"}'
+        else
+            uploads[$i]+=',{"cid":"'"${cids[i]}"'"}'
+        fi
     done
 
     getCmds=""
@@ -22,9 +28,25 @@ body() {
     done
     getCmds+="[1,$((num_nodes-1))] -- ipfs get ${cids[0]}"
 
-    echo -e "$getCmds" | iptb run |
-       perl -p -e 's/node\[(\d)\].*?$/\1/' |
-       sed -e '/^$/d' |
-       grep -oP '((^\d)|(Qm.*?)|([0-9ms]+))$' |
-       sed 'N;N;s/\n/,/g' > "${results_prefix}aggregate"
+    # echo -e "$getCmds" | iptb run |
+    #    perl -p -e 's/node\[(\d)\].*?$/\1/' |
+    #    sed -e '/^$/d' |
+    #    grep -oP '((^\d)|(Qm.*?)|([0-9ms]+))$' |
+    #    sed 'N;N;s/\n/,/g' > "${results_prefix}aggregate"
+
+    # Request files, gather stats
+    declare -Ag dl_times
+    while IFS=$'\n' read -r key; do
+        read -r val
+        if [[ -z ${dl_times[$key]} ]]; then
+            dl_times[$key]=$val
+        else
+            dl_times[$key]+=",$val"
+        fi
+    done <<< $(echo -e "$getCmds" | iptb run |
+               perl -p -e 's/node\[(\d)\].*?$/\1/' |
+               sed -e '/^$/d' |
+               grep -oP '((^\d)|(Qm.*?)|([0-9ms]+))$' |
+               sed -r 'N;N;s/([0-9]+)\n([A-Za-z0-9]+)\n([0-9ms]+)/\1\n{"block":"\2","time":"\3"}/'
+    )
 }
