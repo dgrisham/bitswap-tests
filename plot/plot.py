1b156e894cbdfcf9603a25b1343064a0865db38f
dgrisham <dgrisham@mines.edu>
2018-10-11 18:41:23 -0600

    Pull getPlotConfig() out of plot(); conform to pip standards.

--- plot/plot.py
+++ plot/plot.py
@@ -5,14 +5,10 @@ import sys
 import argparse
 import json
 import traceback
-import warnings
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-from itertools import islice
 from collections import OrderedDict
 from os.path import splitext
 from math import floor, ceil, log10
@@ -22,6 +18,7 @@ from pandas.io.json import json_normalize
 plt.style.use('ggplot')
 rcParams.update({'figure.autolayout': True})
 rcParams['axes.titlepad'] = 4
+
 
 def main(argv):
     cli = argparse.ArgumentParser()
@@ -76,23 +73,26 @@ def main(argv):
         traceback.print_exc()
         sys.exit(1)
 
-    results['ledgers'].index = results['ledgers'].index.map(lambda idx: (idx[0], idx[1], idx[2].total_seconds()))
+    results['ledgers'].index = results['ledgers'].index.map(
+        lambda idx: (idx[0], idx[1], idx[2].total_seconds()))
     time = results['ledgers'].index.levels[2]
     if args.prange is not None:
         ti = floor(args.prange[0] * len(time))
         tf = ceil(args.prange[1] * len(time)) - 1
     elif args.trange is not None:
-        ti, tf = trange
+        ti, tf = args.trange
     else:
         ti, tf = 0, len(time) - 1
     trange = time[[ti, tf]]
 
+    plotCfg = getPlotConfig(results['ledgers'], trange, results['params'],
+                            args.kind)
     try:
         if args.save:
-            plot(results['ledgers'], results['params'], args.kind, trange,
-                outfilePrefix=f'{splitext(args.infile)[0]}-{args.kind}')
+            plot(results['ledgers'], trange, plotCfg,
+                 f'{splitext(args.infile)[0]}-{args.kind}')
         else:
-            plot(results['ledgers'], results['params'], args.kind, trange)
+            plot(results['ledgers'], trange, plotCfg)
         if not args.no_show:
             plt.show()
             plt.clf()
@@ -103,28 +103,41 @@ def main(argv):
 
     return results
 
+
 def load(fname):
     """
     Load json results file into 3 dataframes:
         1.  `uploads`: Set of blocks uploaded by each peer.
-        2.  `dl_times`: Each peers' downloaded times for the blocks they downloaded.
+        2.  `dl_times`: Each peers' downloaded times for the blocks they
+            downloaded.
         3.  `ledgers`: The Bitswap ledger update ledgers for each peer.
     Input:
         -   `fname` :: str: Path to json file to load.
-    Returns: A dictionary containing the above dataframes.
+    Returns:
+        A dictionary containing the above dataframes.
     """
 
     with open(fname, 'r') as jfile:
         jdata = json.load(jfile)
 
     # load results into separate dataframes
-    params = pd.DataFrame.from_records(jdata, exclude=['uploads', 'dl_times', 'history'], index='id')
-    uploads  = pd.concat([json_normalize(data=pdata, record_path='uploads',  meta='id')
-                   for pdata in jdata]).set_index('id')
-    dl_times = pd.concat([json_normalize(data=pdata, record_path='dl_times', meta='id')
-                   for pdata in jdata]).set_index(['id', 'block'])
-    ledgers  = pd.concat([json_normalize(data=pdata, record_path='history',  meta='id')
-                   for pdata in jdata])
+    params = pd.DataFrame.from_records(
+        jdata, exclude=['uploads', 'dl_times', 'history'], index='id')
+
+    uploads = pd.concat([
+        json_normalize(data=pdata, record_path='uploads', meta='id')
+        for pdata in jdata
+    ]).set_index('id')
+
+    dl_times = pd.concat([
+        json_normalize(data=pdata, record_path='dl_times', meta='id')
+        for pdata in jdata
+    ]).set_index(['id', 'block'])
+
+    ledgers = pd.concat([
+        json_normalize(data=pdata, record_path='history', meta='id')
+        for pdata in jdata
+    ])
 
     # use relative times for debt ratio update timestamps
     ledgers['time'] = ledgers['time'].apply(pd.to_datetime)
@@ -132,33 +145,50 @@ def load(fname):
     ledgers['time'] = ledgers['time'].apply(lambda t: t - t0)
     ledgers = ledgers.set_index(['id', 'peer', 'time'])
 
-    return {'params': params, 'uploads': uploads, 'dl_times': dl_times, 'ledgers': ledgers}
+    return {
+        'params': params,
+        'uploads': uploads,
+        'dl_times': dl_times,
+        'ledgers': ledgers
+    }
 
-def plot(ledgers, params, kind, trange, outfilePrefix=None):
+
+def getPlotConfig(ledgers, trange, params, kind):
     """
-    Purpose
-        Plots debt ratios (stored in `ls`, aka ledgers) from either:
-        -   trange[0] to trange[1], or
-        -   prange[0] * tf to prange[1] * tf, where tf is the last time that
-            the ledgers were updated
-        Currently supports 2 kinds of plots:
-        -   'all': Plot every peerwise time series of debt ratio values on one plot. This
-                   will produce one plot with a line for each pair of peers.
-        -   'pairs': Make one time-series plot for every pair of peers i j. Each plot will
-                     contain two lines: one for user i's view of peer j, and one for j's
-                     view of i.
-        Note: Two users are considered 'peers' if at least one of them has a ledger history
-              stored for the other.
+    Get all of the configuration values needed by plot().
+
     Inputs:
-        -   `ls :: pd.DataFrame`
-        -   `kind :: str`: Which plotting function to use.
-        -   `trange :: (pd.Datetime, pd.Datetime)`
-        -   `prange :: (float, float)`
+        -   `ledgers :: pd.DataFrame`
+        -   `trange :: (pd.Datetime, pd.Datetime)`: Time range to plot
+        -   `params :: dict`: Node parameters as loaded in load().
+        -   `kind :: str`: Which type of plot to configure for. Possible
+            values:
+            -   'all': Plot every peerwise time series of debt ratio values on
+                one plot. This will produce one plot with a line for each pair
+                of peers.
+            -   'pairs': Make one time-series plot for each pair of peers i, j.
+                Each plot will contain two lines: one for user i's view of peer
+                j, and one for j's view of i.
+        Note: Two users are considered 'peers' if at least one of them has a
+        ledger history stored for the other.
+
+    Returns:
+        `cfg :: dict`: Dictionary containing the following keys/values:
+            -   `title :: str`: The plot title.
+            -   `num_axes :: int`: The number of sub-plots to make.
+            -   `pairs :: int`: The number of pairs of peers there are to
+                plot. One for every pair of peers that have a history
+                together.
+            -   `cycleLen :: int`: The length of the color cycle for
+                matplotlib.
+            -   `colors :: [str]`: List of the colors to use in the color
+                cycle.
+            -   `colorMap :: dict{(str, str): (str, str)}`: Dictionary that
+                maps from an ordered pair of peers to their corresponding
+                pair of plot colors.
     """
 
     tmin, tmax = trange
-    time = ledgers.index.levels[2]
-
     colorPairs = [
         ('magenta', 'black'),
         ('green', 'orange'),
@@ -166,8 +196,8 @@ def plot(ledgers, params, kind, trange, outfilePrefix=None):
     ]
     colorMap = {}
     colors = []
-    # figure out how many peers have a history in this data range, and assign colors to each
-    # pair
+    # figure out how many peers have a history in this data range, and assign
+    # colors to each pair
     pairs = 0
     for user in ledgers.index.levels[0]:
         u = ledgers.loc[user]
@@ -187,32 +217,77 @@ def plot(ledgers, params, kind, trange, outfilePrefix=None):
     if kind == 'all':
         # only make a single plot axis
         n = 1
-        # the color cycle length is equal to the number of pairs of peers (order matters)
+        # the color cycle length is equal to the number of pairs of peers
+        # (order matters)
         cycleLen = pairs * 2
     elif kind == 'pairs':
         # one plot axis for every peer
         n = pairs // 2
-        # the color cycle length is equal to the number of pairs of peers (order doesn't
-        # matter)
+        # the color cycle length is equal to the number of pairs of peers
+        # (order doesn't matter)
         cycleLen = pairs
 
-    plotTitle = mkTitle(params)
+    return {
+        'title': mkTitle(params),
+        'num_axes': n,
+        'pairs': pairs,
+        'cycleLen': cycleLen,
+        'colors': colors,
+        'colorMap': colorMap,
+    }
+
+
+def mkTitle(params):
+    paramTitles = OrderedDict()
+    paramTitles['strategy'] = 'RF'
+    paramTitles['upload_bandwidth'] = 'BW'
+    paramTitles['round_burst'] = 'RB'
+
+    pts = []
+    for p, t in paramTitles.items():
+        vals = params[p]
+        if vals.nunique() == 1:
+            pts.append(f"{t}: {vals[0].title()}")
+        else:
+            pts.append(f"{t}s: {', '.join(vals).title()}")
+
+    return f"Debt Ratio vs. Time -- {', '.join(pts)}"
+
+
+def plot(ledgers, trange, cfg, outfilePrefix=None):
+    """
+    Plots debt ratios (stored in `ledgers`) from trange[0] to' trange[1].
+    The history time series is plotted as a curve where the y-axis is the
+    debt ratio value. Two concentric circles are plotted at trange[1] for
+    each pair of peers i, j, where the inner circle's radius represents the
+    amount of data j has sent to i and the outer radius represents the
+    amount of data i has sent to j.
+
+    Inputs:
+        -   `ledgers :: pd.DataFrame`
+        -   `trange :: (pd.Datetime, pd.Datetime)`: Time range to plot
+        -   `cfg :: dict`: Plot config. See getPlotConfig().
+        -   `outfilePrefix :: str`: basename of the file to save to (if any)
+    """
+
     try:
-        fig, axes = mkAxes(n, cycleLen, plotTitle, colors)
+        fig, axes = mkAxes(cfg['num_axes'], cfg['cycleLen'], cfg['title'],
+                           cfg['colors'])
     except Exception as e:
         raise prependErr("error configuring plot axes", e)
     try:
-        figLog, axesLog = mkAxes(n, cycleLen, plotTitle, colors, log=True)
+        figLog, axesLog = mkAxes(cfg['num_axes'], cfg['cycleLen'],
+                                 cfg['title'], cfg['colors'], log=True)
     except Exception as e:
         raise prependErr("error configuring semi-log plot axes", e)
 
     drstats = {
-        'min'  : ledgers['value'].min(),
-        'max'  : ledgers['value'].max(),
-        'mean' : ledgers['value'].mean(),
+        'min': ledgers['value'].min(),
+        'max': ledgers['value'].max(),
+        'mean': ledgers['value'].mean(),
     }
     plotTRange(ledgers, trange, axes, axesLog, 'curve', drstats=drstats)
-    plotTRange(ledgers, trange, axes, axesLog, 'dot', colorMap=colorMap)
+    plotTRange(ledgers, trange, axes, axesLog, 'dot', colorMap=cfg['colorMap'])
     try:
         cfgAxes(axes)
     except Exception as e:
@@ -231,7 +306,21 @@ def plot(ledgers, params, kind, trange, outfilePrefix=None):
         pdfOutLog = f'{outfilePrefix}-semilog.pdf'
         figLog.savefig(pdfOutLog, bbox_inches='tight')
 
+
 def plotTRange(ledgers, trange, axes, axesLog, kind, **kwargs):
+    """
+    Inputs:
+        -   `ledgers :: pd.DataFrame`
+        -   `trange :: (pd.Datetime, pd.Datetime)`: Time range to plot
+        -   `axes :: [matplotlib.axes]`
+        -   `axesLog :: [matplotlib.axes]`
+        -   `kind :: str`: Which plot to make. Possible values:
+            -   'curve': Plot the time series curve from trange[0] to
+                trange[1].
+            -   'dot': Plot the dot at trange[1].
+        -   `kwargs :: dict`: Keyword arguments for wrapped plot functions.
+    """
+
     tmin, tmax = trange
     # k is the index of the axis we should be plotting on
     k = 0
@@ -247,7 +336,8 @@ def plotTRange(ledgers, trange, axes, axesLog, kind, **kwargs):
 
             if kind == 'curve':
                 if len(p) == 0:
-                    warn(f"no data for peers {i} ({user}) and {j} ({peer}) in [{tmin}, {tmax}]")
+                    warn(f"no data for peers {i} ({user}) and {j} ({peer}) in "
+                         f"[{tmin}, {tmax}]")
                     continue
                 plotCurve(p, trange, i, j, ax, axLog, **kwargs)
             elif kind == 'dot':
@@ -260,14 +350,17 @@ def plotTRange(ledgers, trange, axes, axesLog, kind, **kwargs):
         extendAxis(ax, kwargs['extend'])
         extendAxis(axLog, kwargs['extend'], log=True)
 
+
 def plotCurve(p, trange, i, j, ax, axLog, drstats):
     factor = 0.25
     xmin, xmax = trange
-    ymin, ymax = drstats['min'] - factor*drstats['mean'], drstats['max'] + factor*drstats['mean']
+    ymin, ymax = drstats['min'] - factor * \
+        drstats['mean'], drstats['max'] + factor*drstats['mean']
     p.plot(y='value', xlim=(xmin, xmax), ylim=(ymin, ymax), ax=ax,
-       label=f"Debt ratio of {j} wrt {i}")
+           label=f"Debt ratio of {j} wrt {i}")
     p.plot(y='value', xlim=(xmin, xmax), logy=True, ax=axLog,
-       label=f"Debt ratio of {j} wrt {i}")
+           label=f"Debt ratio of {j} wrt {i}")
+
 
 def plotDot(p, user, peer, ax, axLog, colorMap, extend=0):
     inner = p.iloc[[-1]]
@@ -281,15 +374,16 @@ def plotDot(p, user, peer, ax, axLog, colorMap, extend=0):
     msize = 3
     cInner, cOuter = colorMap[user, peer]
     ax.plot(t, d, color=cOuter, marker='o', markersize=(ri+ro)*msize,
-        markeredgecolor='black')
+            markeredgecolor='black')
     ax.plot(t, d, color=cInner, marker='o', markersize=ri*msize,
-        markeredgecolor='black')
+            markeredgecolor='black')
     axLog.plot(t, d, color=cOuter, marker='o', markersize=(ri+ro)*msize,
-        markeredgecolor='black')
+               markeredgecolor='black')
     axLog.plot(t, d, color=cInner, marker='o', markersize=ri*msize,
-        markeredgecolor='black')
+               markeredgecolor='black')
 
     return max(extend, (ri+ro)*msize/2)
+
 
 def extendAxis(ax, amt, log=False):
     xright = ax.get_xlim()[1]
@@ -299,31 +393,18 @@ def extendAxis(ax, amt, log=False):
         # TODO: need a better way to do this
         ax.set_ylim(bottom=ybottom-amt*50000)
 
-def mkTitle(params):
-    paramTitles = OrderedDict()
-    paramTitles['strategy']         = 'RF'
-    paramTitles['upload_bandwidth'] = 'BW'
-    paramTitles['round_burst']      = 'RB'
-
-    pts = []
-    for p, t in paramTitles.items():
-        vals = params[p]
-        if vals.nunique() == 1:
-            pts.append(f"{t}: {vals[0].title()}")
-        else:
-            pts.append(f"{t}s: {', '.join(vals).title()}")
-
-    return f"Debt Ratio vs. Time -- {', '.join(pts)}"
 
 def mkAxes(n, cycleLen, plotTitle, colors, log=False):
     """
     Create and configure `n` axes for a given debt ratio plot.
+
     Inputs:
         -   `n :: int`: Number of sub-plots to create.
         -   `plotTitle :: str`: Title of this plot.
         -   `log :: bool`: Whether the y-axis will be logarithmic.
+
     Returns:
-        -   `[Axes]`: List containing the `n` axes.
+        `[matplotlib.axes]`: List containing the `n` axes.
     """
 
     fig, axes = plt.subplots(n)
@@ -331,31 +412,32 @@ def mkAxes(n, cycleLen, plotTitle, colors, log=False):
         axes = [axes]
 
     for i, ax in enumerate(axes):
-        ax.set_prop_cycle('color', colors[2*i : 2*i + cycleLen])
+        ax.set_prop_cycle('color', colors[2*i: 2*i + cycleLen])
 
         if n > 1:
-            # if there are multiple plots in this figure, give each one a unique subtitle
+            # if there are multiple plots in this figure, give each one a
+            # unique subtitle
             title = f"User {i}"
-            axArgs = { 'fontsize' : 'medium',
-                       'bbox': { 'boxstyle'  : 'round',
-                                 'facecolor' : ax.get_facecolor(),
-                                 'edgecolor' : '#000000',
-                                 'linewidth' : 1,
-                                },
-                     }
+            axArgs = {'fontsize': 'medium',
+                      'bbox': {'boxstyle': 'round',
+                               'facecolor': ax.get_facecolor(),
+                               'edgecolor': '#000000',
+                               'linewidth': 1,
+                               },
+                      }
             ax.set_title(title, **axArgs)
 
         ylabel = "Debt Ratio"
-        titleArgs = { 'fontsize' : 'large',
-                      'x'        : (ax.get_position().xmin+ax.get_position().xmax) / 2,
-                      'y'        : 1.02,
-                      'ha'       : 'center',
-                      'bbox': { 'boxstyle' : 'round',
-                               'facecolor' : ax.get_facecolor(),
-                               'edgecolor' : '#000000',
-                               'linewidth' : 1,
+        titleArgs = {'fontsize': 'large',
+                     'x': (ax.get_position().xmin+ax.get_position().xmax) / 2,
+                     'y': 1.02,
+                     'ha': 'center',
+                     'bbox': {'boxstyle': 'round',
+                              'facecolor': ax.get_facecolor(),
+                              'edgecolor': '#000000',
+                              'linewidth': 1,
                               },
-                    }
+                     }
         if log:
             ax.set_ylabel(f"log({ylabel})")
             fig.suptitle(f"{plotTitle} (Semi-Log)", **titleArgs)
@@ -367,10 +449,11 @@ def mkAxes(n, cycleLen, plotTitle, colors, log=False):
 
     return fig, axes
 
+
 def cfgAxes(axes, log=False, **kwargs):
     """
-    Configure axes settings that must be set after plotting (e.g. because the
-    pandas plotting function overwrites them).
+    Configure axes settings that must be set after plotting (e.g. because
+    the pandas plotting function overwrites them).
     """
 
     for i, ax in enumerate(axes):
@@ -385,13 +468,17 @@ def cfgAxes(axes, log=False, **kwargs):
             ax.set_ylim(top=kwargs['ymax']*1.5)
             if len(axes) > 1:
                 yticks = ax.get_yticks()
-                ax.set_yticks([yticks[i] for i in range(len(yticks)) if i % 2 == 0])
+                ax.set_yticks([yticks[i]
+                               for i in range(len(yticks)) if i % 2 == 0])
+
 
 def warn(msg):
     print(f"warning: {msg}", file=sys.stderr)
 
+
 def prependErr(msg, e):
     return type(e)(f"error {msg}: {e}").with_traceback(sys.exc_info()[2])
+
 
 if __name__ == '__main__':
     r = main(sys.argv[1:])
